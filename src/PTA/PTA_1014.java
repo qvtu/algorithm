8c63c6fea1e14e455eb6643818ffee9ca8daf07f
kkoo <1451108685@qq.com>
2021-11-18 23:50:41 +0800

    "数素数"

---
+++ src/PTA/PTA_1014.java
@@ -0,0 +1,19 @@
+package PTA;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * Copyright (C), 2019-2021, Kkoo
+ * Author: kkoo
+ * Date: 2021/11/18 10:39 下午
+ * FileName: PTA_1014
+ */
+public class PTA_1014 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        String[] arr = new String[4];
+        String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+    }
+}
