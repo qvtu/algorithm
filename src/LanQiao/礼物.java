851eca6b725c6fbed77b81f98dee4ac28d2f10a7
kkoo <1451108685@qq.com>
2021-11-22 23:39:36 +0800

    "礼物"

---
+++ src/LanQiao/礼物.java
@@ -0,0 +1,16 @@
+package LanQiao;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * Copyright (C), 2019-2021, Kkoo
+ * Author: Kkoo
+ * Date: 2021/11/22 0022 23:39
+ * FileName: 礼物
+ */
+public class 礼物 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+    }
+}
