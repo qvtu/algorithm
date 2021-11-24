c6d960d63c5fd8da2af6781b0bf16a2854037be5
kkoo <1451108685@qq.com>
2021-11-24 21:19:08 +0800

    "快速排序"

--- src/Template/快速排序_升序.java
+++ src/Template/快速排序_升序.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 /**
  * Copyright (C), 2019-2021, Kkoo
  * Author: Kkoo
- * Date: 2021/11/24 0024 14:36
- * FileName: 快速排序
+ * Date: 2021/11/24 0024 15:01
+ * FileName: 快速排序_升序
  */
 public class 快速排序_升序 {
     public static void main(String[] args) {
@@ -15,31 +15,39 @@ public class 快速排序_升序 {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sx(int[] arr, int low, int high) {
-        int t;
-        int i = low;
-        int j = high;
-        int temp = arr[low];
+    public static void sx(int[] arr, int left, int right) {
+        //左右两个哨兵
+        int i = left;
+        int j = right;
+        //将 arr[left] 作为基准数
+        int temp = arr[left];
+        //当哨兵没有相遇时运行
         while (i < j) {
+            //先从右往左走 寻找比基准数小的数 i为这个数的下标
             while (i < j && temp <= arr[j]) {
                 j--;
             }
+            //再从左往右走 寻找比基准数大的数 j为这个数的下标
             while (i < j && temp >= arr[i]) {
                 i++;
             }
+            //交换位置
             if (i < j) {
-                t = arr[j];
+                int t = arr[j];
                 arr[j] = arr[i];
                 arr[i] = t;
             }
         }
-        arr[low]=arr[i];
+        //将 基准数 和 arr[i] 交换
+        arr[left] = arr[i];
         arr[i] = temp;
-        if (low < i) {
-            sx(arr, low, i - 1);
+        //通过上面的操作 将数组分为两部分 左半部分小于arr[i] 右边部分大于arr[i]
+        //对分治成两部分的数组再次进行分治
+        if (left < i) {
+            sx(arr, left, i - 1);
         }
-        if (i < high) {
-            sx(arr, i + 1, high);
+        if (i < right) {
+            sx(arr, i + 1, right);
         }
     }
 }
--- src/Template/快速排序_降序.java
+++ src/Template/快速排序_降序.java
@@ -6,20 +6,20 @@ import java.util.Arrays;
  * Copyright (C), 2019-2021, Kkoo
  * Author: Kkoo
  * Date: 2021/11/24 0024 14:36
- * FileName: 快速排序
+ * FileName: 快速排序_降序
  */
 public class 快速排序_降序 {
+    //降序就把升序反过来
     public static void main(String[] args) {
         int[] arr = {324, 23, 4, 234, 2, 35234, 54, 2314, 12, 3, 123, 12, 4, 32, 54, 235, 34, 5, 234, 231};
         jx(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void jx(int[] arr, int high, int low) {
-        int t;
-        int i = high;
-        int j = low;
-        int temp = arr[high];
+    public static void jx(int[] arr, int right, int left) {
+        int i = right;
+        int j = left;
+        int temp = arr[right];
         while (i < j) {
             while (i < j && temp >= arr[j]) {
                 j--;
@@ -28,18 +28,18 @@ public class 快速排序_降序 {
                 i++;
             }
             if (i < j) {
-                t = arr[i];
+                int t = arr[i];
                 arr[i] = arr[j];
                 arr[j] = t;
             }
         }
-        arr[high]=arr[i];
+        arr[right] = arr[i];
         arr[i] = temp;
-        if (high < i) {
-            jx(arr, high, i - 1);
+        if (right < i) {
+            jx(arr, right, i - 1);
         }
-        if (i < low) {
-            jx(arr, i + 1, low);
+        if (i < left) {
+            jx(arr, i + 1, left);
         }
     }
 }
