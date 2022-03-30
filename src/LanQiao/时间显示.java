ab8cc8c86040f9047ded6dfbe404d3664b6cd950
kkoo <1451108685@qq.com>
2022-03-30 23:11:52 +0800

    ""

---
+++ src/LanQiao/时间显示.java
@@ -0,0 +1,24 @@
+package LanQiao;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * Copyright (C), 2019-2022, Kkoo
+ * Author: Kkoo
+ * Date: 2022/3/30 0030 19:17
+ * FileName: 时间显示
+ */
+public class 时间显示 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        long time = in.nextLong();
+        long h, m, s;
+        time /= 1000;
+        time = time % (24 * 60 * 60);
+        h = time / 60 / 60;
+        m = time / 60 % 60;
+        s = time % 60;
+        System.out.printf("%02d:%02d:%02d",h,m,s);
+    }
+}
